b6b147ff514edfec624cfd705049d21f0ed8a14c
ALIANGER <MSHOKRALAHZADE@GMAIL.COM>
2016-08-16 19:40:17 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {54501247},
+    sudo_users = {MROAD_CM2},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
